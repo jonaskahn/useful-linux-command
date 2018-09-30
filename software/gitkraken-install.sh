fc964756034187fa378260ae17f9334fcf957b75
Tuyen <tuyendev@gmail.com>
2018-09-30 15:12:58 +0700

    Fix issues

--- software/gitkraken-install.sh
+++ software/gitkraken-install.sh
@@ -2,16 +2,19 @@
 
 # Enter /opt folder (common folder for user installed programs)
 # This script assumes you have proper permissions on /opt
-cd /opt
+cd /tmp
 
 # Download GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.tar.gz
 
 # Extract the Kraken into /opt directory
-tar -xvzf gitkraken-amd64.tar.gz
+sudo rm -rf /opt/gitkraken
+sudo mkdir /opt/gitkraken
+
+sudo tar -xvzf gitkraken-amd64.tar.gz -C /opt
 
 # Remove tar.gz
-rm gitkraken-amd64.tar.gz
+rm -rf gitkraken-amd64.tar.gz*
 
 # Add gitkraken to PATH
 echo "export PATH=\$PATH:/opt/gitkraken" >> ~/.bashrc
@@ -19,7 +22,8 @@ source ~/.bashrc
 
 # Download gitkraken launcher icon
 wget http://img.informer.com/icons_mac/png/128/422/422255.png
-mv 422255.png ./gitkraken/icon.png
+sudo mv 422255.png /opt/gitkraken/icon.png
+rm -rf 422255.png
 
 # Create desktop entry
 sudo touch /usr/share/applications/gitkraken.desktop
@@ -38,5 +42,5 @@ Categories=Utility;Development;" | sudo tee -a /usr/share/applications/gitkraken
 
 # Adding required lib
 cd /opt/gitkraken
-ln -s /usr/lib64/libcurl.so.4 libcurl.so.4
-ln -s /usr/lib64/libcurl.so.4 /usr/lib64/libcurl-gnutls.so.4
+sudo ln -sf /usr/lib64/libcurl.so.4 libcurl.so.4
+sudo ln -sf /usr/lib64/libcurl.so.4 /usr/lib64/libcurl-gnutls.so.4
