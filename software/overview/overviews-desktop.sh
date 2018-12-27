eab5b26bd8897df020a2f8db0ee7ea032e8963ae
Tuyền Nguyễn <tuyendev@gmail.com>
2018-12-27 20:30:59 +0700

    Fix bugs

--- software/overview/overviews-desktop.sh
+++ software/overview/overviews-desktop.sh
@@ -1,10 +1,10 @@
 cd /tmp
 sudo wget https://raw.githubusercontent.com/tuyendev/useful-linux-command/master/software/overview/overviews.png
-sudo mkdir /opt/overview
-sudo mv overviews.png /opt/overview
-sudo touch /usr/bin/overview
-sudo chmod a+x /usr/bin/overview
-sudo touch /usr/share/applications/overviews.desktop
+sudo mkdir -p /opt/overview
+sudo mv -f overviews.png /opt/overview
+sudo touch -f /usr/bin/overview
+sudo chmod -f a+x /usr/bin/overview
+sudo touch -f /usr/share/applications/overviews.desktop
 echo "dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.overview.show();'" | sudo tee -a /usr/bin/overview > /dev/null
 echo "
 [Desktop Entry]
