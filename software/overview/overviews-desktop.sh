293d05bfcb57e76a727b15133b84967b3f77165e
Tuyền Nguyễn <tuyendev@gmail.com>
2018-12-27 21:37:06 +0700

    move to local app

--- software/overview/overviews-desktop.sh
+++ software/overview/overviews-desktop.sh
@@ -1,18 +1,39 @@
+
+cd ~
+foo=$(systemd-path user)
+mkdir -p ~/.local/.overview
+mkdir -p ~/.local/bin/
+mkdir -p ~/.local/share/applications
+
+
 cd /tmp
-sudo wget https://raw.githubusercontent.com/tuyendev/useful-linux-command/master/software/overview/overviews.png
-sudo mkdir -p /opt/overview
-sudo mv -f overviews.png /opt/overview
-sudo touch -f /usr/bin/overview
-sudo chmod -f a+x /usr/bin/overview
-sudo touch -f /usr/share/applications/overviews.desktop
-echo "dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.overview.show();'" | sudo tee -a /usr/bin/overview > /dev/null
+wget https://raw.githubusercontent.com/tuyendev/useful-linux-command/master/software/overview/overviews.png
+mv -f overviews.png ~/.local/.overview
+
+rm -f ~/.local/bin/showoverview
+touch ~/.local/bin/showoverview
+rm -f ~/.local/bin/hideoverview
+touch ~/.local/bin/hideoverview
+rm -f ~/.local/share/applications/overviews.desktop
+touch ~/.local/share/applications/overviews.desktop
+chmod +x ~/.local/bin/showoverview
+chmod +x ~/.local/bin/hideoverview
+
+echo "foo=$(systemd-path user)
+sed -i 's/showoverview/hideoverview/' $foo/.local/share/applications/overviews.desktop yarly
+dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.overview.show();'" | tee -a ~/.local/bin/showoverview > /dev/null
+
+echo "foo=$(systemd-path user)
+sed -i 's/hideoverview/showoverview/' $foo/.local/share/applications/overviews.desktop yarly 
+dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'Main.overview.hide();'" | tee -a ~/.local/bin/hideoverview > /dev/null
+
 echo "
 [Desktop Entry]
 Name=Overview
 Comment=Show all windows
-Exec=overview &
-Icon=/opt/overview/overviews.png
+Exec=showoverview
+Icon=$foo/.local/.overview/overviews.png
 Terminal=false
 Type=Application
 Encoding=UTF-8
-Categories=Others;" | sudo tee -a /usr/share/applications/overviews.desktop >/dev/null
+Categories=Others;" | tee -a ~/.local/share/applications/overviews.desktop >/dev/null
