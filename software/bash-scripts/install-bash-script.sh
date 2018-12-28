65e8c61c772b9c1f8f11b40567d9807501fd23af
[UP] Tuyen <tuyen.nguyenthanh@axonactive.com>
2018-12-28 14:26:22 +0700

    Display color

--- software/bash-scripts/install-bash-script.sh
+++ software/bash-scripts/install-bash-script.sh
@@ -2,21 +2,28 @@ cd ~
 foo=$(systemd-path user)
 rm -f ~/.local/bin/bs
 touch ~/.local/bin/bs
-echo "if [ \"\$1\" = "" ];
+echo "
+RED='\e[41m'
+GREEN='\e[102m'
+BLUE='\e[104m'
+YELLOW='\e[103m'
+NC='\e[49m'
+
+if [ \"\$1\" = "" ];
 then echo "Do nothing"
 else
     for d in *;
         do
-            if [ -d "$d" ]; then
+            if [ -d "$d" ] ; then
                 cd "$d"
                 echo ""
-                echo "Project $d will run: $1"
-                echo ""
+                echo -e \"Project \${RED}$d \${NC}will run \${NC}: \${GREEN}$1\${NC}\"
+                echo -e \"\"
                 $1
                 cd ..
                 echo ""
-                echo "*************************"
-                echo ""
+                echo -e \"\${BLUE}END\${NC}\"
+                echo -e \"\"
             fi
         done
 fi" | tee -a  ~/.local/bin/bs > /dev/null
