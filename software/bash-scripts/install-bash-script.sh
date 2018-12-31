0344b8a39bc0efccccc2693b65efac7a54f29103
[UP] Tuyen <tuyen.nguyenthanh@axonactive.com>
2018-12-31 10:32:41 +0700

    Fixing issues

--- software/bash-scripts/install-bash-script.sh
+++ software/bash-scripts/install-bash-script.sh
@@ -3,27 +3,28 @@ foo=$(systemd-path user)
 rm -f ~/.local/bin/bs
 touch ~/.local/bin/bs
 echo "
+#!/bin/bash
 RED='\e[41m'
 GREEN='\e[102m'
 BLUE='\e[104m'
 YELLOW='\e[103m'
 NC='\e[49m'
 
-if [ \"\$1\" = "" ];
-then echo "Do nothing"
+if [ \"\$1\" = \"\" ];then 
+    echo \"Do nothing\"
 else
     for d in *;
         do
-            if [ -d "$d" ] ; then
-                cd "$d"
+            if [ -d \"\$d\" ]; then
+                cd \"\$d\"
                 echo ""
-                echo -e \"Project \${RED}$d \${NC}will run \${NC}: \${GREEN}$1\${NC}\"
-                echo -e \"\"
-                $1
+                echo -e \"Project \${RED}\$d \${NC}will run \${NC}: \${YELLOW} \$1 \${NC}\"
+                echo \"\"
+                \$1
                 cd ..
-                echo ""
+                echo \"\"
                 echo -e \"\${BLUE}END\${NC}\"
-                echo -e \"\"
+                echo \"\"
             fi
         done
 fi" | tee -a  ~/.local/bin/bs > /dev/null
